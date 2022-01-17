4ad637ce0df72809adac7f234fd8891b2803fe24
ChloeArd <chloe.ardoise@gmail.com>
2022-01-17 11:25:00 +0100

    Initial commit

---
+++ public/build/js/front.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*************************!*\
+  !*** ./assets/front.ts ***!
+  \*************************/
+
+
+/******/ })()
+;
+//# sourceMappingURL=front.js.map
